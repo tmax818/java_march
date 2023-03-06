154eecc5368e91486aaef0f2074fdf36572f1576
tmax818 <tylermaxwell661@gmail.com>
2023-03-06 12:28:30 -0800

    update

---
+++ algos/day01/Basic13.java
@@ -0,0 +1,71 @@
+package algos.day01;
+
+public class Basic13 {
+
+/**
+* Print all the integers from 1 to 255.
+ * 
+ */
+
+public void Basic01(){}
+
+/**
+ * Print integers from 0 to 255, and with each integer print the sum so far.
+ */
+public void Basic02(){}
+
+
+/**
+ * Given an array, find and print its largest element.
+ */
+public void Basic03(){}
+/**
+ * Create an array with all the odd integers between 1 and 255 (inclusive).
+
+ */
+public void Basic04(){}
+/**
+ * Given an array and a value Y, count and print the number of array values greater than Y.
+ */
+public void Basic05(){}
+/**
+ * Given an array, print the max, min and average values for that array.
+ */
+public void Basic06(){}
+/**
+ * Replace any negative array values with 'Dojo'.
+ */
+public void Basic07(){}
+/**
+ * Print all odd integers from 1 to 255.
+ */
+public void Basic08(){}
+/**
+ * Iterate through a given array, printing each value.
+ */
+public void Basic09(){}
+/**
+ * Analyze an array’s values and print the average
+ */
+public void Basic10(){}
+/**
+ * Square each value in a given array, returning that same array with changed values
+ */
+public void Basic11(){}
+/**
+ * Return the given array, after setting any negative values to zero.
+ */
+public void Basic12(){}
+/**
+ * Given an array, move all values forward by one index, dropping the first and leaving a '0' value at the end.
+ */
+public void BasicLast(){}
+
+
+
+    
+
+
+
+    
+}
---
+++ algos/day01/Main.java
@@ -0,0 +1,9 @@
+package algos.day01;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+    }
+}
