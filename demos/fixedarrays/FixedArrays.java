e4e33518f06d03314693e61d6c7a6e85787a87ad
tmax818 <tylermaxwell661@gmail.com>
2023-03-06 07:49:51 -0800

    update

--- demos/fixedarrays/FixedArrays.java
+++ demos/fixedarrays/FixedArrays.java
@@ -5,15 +5,13 @@ public class FixedArrays {
     public static void main(String[] args) {
         
         // fixed array of ints
-        int[] intArray = new int[5];
-        System.out.println(intArray);
+
         
         // fixed array of doubles
-        double[] doubleArray = new double[10];
+
         
         // fixed array of strings
-        String[] stringArray = new String[4];
-        String[] stringArrayEasy = {"Rick", "Morty", "Summer", "Beth"};
+
     }
 }
     
---
+++ demos/strings/Strings.java
@@ -0,0 +1,16 @@
+package demos.strings;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        // create a string
+
+        // .concat two strings
+
+        // use .format
+
+
+
+    }
+    
+}
