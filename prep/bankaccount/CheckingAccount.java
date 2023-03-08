86dd9b679133a76e3fec7b5d696988100cfce8e5
tmax818 <tylermaxwell661@gmail.com>
2023-03-08 07:42:54 -0800

    update

---
+++ prep/bankaccount/CheckingAccount.java
@@ -0,0 +1,14 @@
+package prep.bankaccount;
+
+public class CheckingAccount extends BankAccount {
+
+    public CheckingAccount(double balance, double intRate) {
+        super(balance, intRate);
+
+    }
+
+
+
+
+    
+}
