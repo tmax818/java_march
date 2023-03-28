e342658ac9e57a38ad5d9cfdbd56e411063bc83c
tmax818 <tylermaxwell661@gmail.com>
2023-03-28 07:44:43 -0700

    update

--- algos/day14/index.js
+++ algos/day14/index.js
@@ -21,7 +21,10 @@ class Stack {
      * @param {any} item The new item to be added to the top / back.
      * @returns {number} The new length of this stack.
      */
-    push(item) {}
+    push(item) {
+      this.items.push(item);
+      return this.size();
+    }
   
     /**
      * Removes the top / last item from this stack.
@@ -29,7 +32,9 @@ class Stack {
      * - Space: O(1) constant.
      * @returns {any} The removed item or undefined if this stack was empty.
      */
-    pop() {}
+    pop() {
+      return this.items.pop()
+    }
   
     /**
      * Retrieves the top / last item from this stack without removing it.
@@ -37,7 +42,9 @@ class Stack {
      * - Space: O(1) constant.
      * @returns {any} The top / last item of this stack.
      */
-    peek() {}
+    peek() {
+      return this.items[this.items.length - 1]
+    }
   
     /**
      * Returns whether or not this stack is empty.
@@ -45,7 +52,9 @@ class Stack {
      * - Space: O(1) constant.
      * @returns {boolean}
      */
-    isEmpty() {}
+    isEmpty() {
+      return this.items.length === 0;
+    }
   
     /**
      * Returns the size of this stack.
@@ -53,7 +62,9 @@ class Stack {
      * - Space: O(1) constant.
      * @returns {number} The length.
      */
-    size() {}
+    size() {
+      return this.items.length;
+    }
   }
   
   class StackNode {
@@ -67,4 +78,82 @@ class Stack {
     constructor() {
       this.head = null;
     }
+
+    push(val){
+      const newNode = new StackNode(val)
+
+      if(this.isEmpty()){
+        this.head = newNode
+      } else {
+
+        let runner = this.head
+        while(runner.next){
+          runner = runner.next
+        }
+        console.log(runner.data)
+        runner.next = newNode
+      }
+    }
+    
+
+
+    pop(){
+      if(this.isEmpty()){
+        return null;
+      } else {
+        var runner = this.head
+        var prev = this.head
+        while(runner.next){
+          
+          runner = runner.next
+        }
+        var value = runner.data
+      }
+      runner = null
+      return value
+    }
+
+
+    peek(){
+      if(this.isEmpty()){
+        return null;
+      }
+
+      let runner = this.head
+      while(runner.next){
+        runner = runner.next
+      }
+      return runner.data
+    }
+
+
+    isEmpty(){
+      return this.head === null;
+    }
+
+
+    size(){}
   }
+
+
+const ourStack = new LinkedListStack();
+
+ourStack.push(12)
+ourStack.push(23)
+ourStack.push(42)
+ourStack.push(56)
+console.log(ourStack.pop())
+console.log(ourStack.peek())
+
+
+
+
+
+
+// console.log(ourStack.isEmpty());
+// ourStack.push(1);
+// ourStack.push(2);
+// ourStack.push(3);
+// ourStack.push(4);
+// console.log(ourStack.peek());
+// console.log(ourStack.isEmpty());
